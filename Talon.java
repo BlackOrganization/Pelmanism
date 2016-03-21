41974fb16fbd611f5ccf2edb1cb82d7014237aa7
松田卓也 <Taku@Taku.local>
2016-03-22 01:48:50 +0900

    Player, StartGame, Talonの3つのクラスを作成

---
+++ Talon.java
@@ -0,0 +1,8 @@
+public class Talon {
+	private String[] num = {
+		"A","2","3","4","5","6","7","8","9","10","J","Q","K",
+	};
+	private static final String[] name = {
+　　		"spade","hart","club","diamond", 
+	};
+}
